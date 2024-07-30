9408dcb856eff4c80697ef974d992ddcbdafec56
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2024-07-30 20:33:01 +0330

    improved type hints

--- asyncpg_client/client.py
+++ asyncpg_client/client.py
@@ -1,6 +1,6 @@
 import contextlib
 import asyncio
-from typing import Dict, Optional
+from typing import Dict, Optional, AsyncIterator
 
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
@@ -18,59 +18,53 @@ class AsyncPostgres:
     _locks: Dict[str, asyncio.Lock] = {}
 
     def __new__(cls, config: PostgresConfig, *args, **kwargs) -> 'AsyncPostgres':
-        url = config.async_url
+        url: str = config.async_url
         if url not in cls._locks:
             cls._locks[url] = asyncio.Lock()
-        return cls._instances.get(url, None) or super().__new__(cls)
+        if url not in cls._instances:
+            cls._instances[url] = super().__new__(cls)
+        return cls._instances[url]
 
     def __init__(self, config: PostgresConfig) -> None:
         if not hasattr(self, '_initialized') or not self._initialized:
-            self._config = config
+            self._config: PostgresConfig = config
             self._async_engine: Optional[AsyncEngine] = None
-            self._async_session_maker: Optional[async_sessionmaker] = None
-            self._initialized = True
+            self._async_session_maker: Optional[async_sessionmaker[AsyncSession]] = None
+            self._initialized: bool = True
 
     @classmethod
-    async def create(
-        cls,
-        config: Optional[PostgresConfig] = None,
-        **kwargs,
-    ) -> 'AsyncPostgres':
+    async def create(cls, config: Optional[PostgresConfig] = None, **kwargs) -> 'AsyncPostgres':
         if config is None:
             config = PostgresConfig(**kwargs)
-
-        url = config.async_url
-        if url not in cls._instances:
-            cls._locks[url] = asyncio.Lock()
-            async with cls._locks[url]:
-                if url not in cls._instances:
-                    pg_instance = cls(config)
-                    await pg_instance.connect()
-                    cls._instances[url] = pg_instance
-
+        url: str = config.async_url
+        async with cls._locks[url]:
+            if url not in cls._instances:
+                instance = cls(config)
+                await instance._initialize_connection()
+                cls._instances[url] = instance
         return cls._instances[url]
 
-    @property
-    def url(self) -> str:
-        return self._config.async_url
+    async def _initialize_connection(self) -> None:
+        await self._init_engine_and_session()
+        await self._test_connection()
 
-    async def init(self) -> None:
+    async def _init_engine_and_session(self) -> None:
         if self._async_engine is None:
             self._async_engine = self._create_async_engine()
             self._async_session_maker = self._create_async_session_maker()
 
     def _create_async_engine(self) -> AsyncEngine:
         return create_async_engine(
-            url=self.url,
+            url=self._config.async_url,
             echo=self._config.echo_log,
             pool_size=self._config.pool_size,
             max_overflow=self._config.pool_overflow,
             pool_timeout=self._config.timeout,
         )
 
-    def _create_async_session_maker(self) -> async_sessionmaker:
+    def _create_async_session_maker(self) -> async_sessionmaker[AsyncSession]:
         if not self._async_engine:
-            raise PGEngineInitializationError(url=self.url)
+            raise PGEngineInitializationError(url=self._config.async_url)
         return async_sessionmaker(
             bind=self._async_engine,
             expire_on_commit=self._config.expire_on_commit,
@@ -79,10 +73,10 @@ class AsyncPostgres:
         )
 
     @contextlib.asynccontextmanager
-    async def get_or_create_session(self) -> AsyncSession:
-        await self.init()
+    async def get_or_create_session(self) -> AsyncIterator[AsyncSession]:
+        await self._init_engine_and_session()
         if not self._async_session_maker:
-            raise PGSessionCreationError(url=self.url)
+            raise PGSessionCreationError(url=self._config.async_url)
 
         async with self._async_session_maker() as session:
             try:
@@ -91,17 +85,16 @@ class AsyncPostgres:
                     await session.commit()
             except SQLAlchemyError as e:
                 await session.rollback()
-                raise PGSessionCreationError(url=self.url, message=str(e))
+                raise PGSessionCreationError(url=self._config.async_url, message=str(e))
             finally:
                 await session.close()
 
-    async def connect(self) -> None:
-        await self.init()
+    async def _test_connection(self) -> None:
         try:
             async with self._async_engine.begin() as connection:
                 await connection.run_sync(lambda conn: None)
         except SQLAlchemyError as e:
-            raise PGConnectionError(url=self.url, message=str(e))
+            raise PGConnectionError(url=self._config.async_url, message=str(e))
 
     async def disconnect(self) -> None:
         if self._async_engine:
@@ -110,4 +103,8 @@ class AsyncPostgres:
 
     async def reconnect(self) -> None:
         await self.disconnect()
-        await self.connect()
+        await self._initialize_connection()
+
+    @property
+    def url(self) -> str:
+        return self._config.async_url
